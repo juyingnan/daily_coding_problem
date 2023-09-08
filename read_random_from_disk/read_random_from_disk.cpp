badb83c12cf5bb670561264040bba579da5b3665
juyingnan <4323145+juyingnan@users.noreply.github.com>
2023-09-08 14:55:36 -0700

    updated the parameters

--- read_random_from_disk/read_random_from_disk.cpp
+++ read_random_from_disk/read_random_from_disk.cpp
@@ -4,39 +4,52 @@
 
 using namespace std;
 
-void read_file(ifstream& file, int position, int n, string& content) 
+void read_file(ifstream& file, int position, int n, string& content)
 {
     file.seekg(position);
     content.resize(n);
     file.read(&content[0], n);
 }
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    string filename;
-    if (argc == 2) 
-        filename = argv[1];
-    else 
-        filename = "random_file.txt"; // default filename
+    string filename = "random_file.txt"; // default filename
+    int default_n = 4096;                // default number of characters to read
+
+    for (int i = 1; i < argc; i++)
+    {
+        string arg = argv[i];
+        if (arg == "-f" && i + 1 < argc)
+        {
+            filename = argv[i + 1];
+            i++;
+        }
+        else if (arg == "-n" && i + 1 < argc)
+        {
+            default_n = stoi(argv[i + 1]);
+            i++;
+        }
+    }
+
     ifstream file(filename);
-    if (!file.is_open()) 
+    if (!file.is_open())
     {
         cout << "Error: Cannot open file " << filename << endl;
         exit(EXIT_FAILURE);
     }
+
     int position = 0;
-    int default_n = 4096;
-    while (true) 
+    while (true)
     {
         string input;
         cout << "Enter the number of characters to display (default is 4k, -1 to exit): ";
         getline(cin, input);
         int n;
-        if (input.empty()) 
+        if (input.empty())
             n = default_n;
-        else 
+        else
             n = stoi(input);
-        if (n == -1) 
+        if (n == -1)
             break;
         string next_text;
         read_file(file, position, n, next_text);
