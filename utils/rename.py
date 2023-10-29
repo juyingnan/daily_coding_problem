1b9a6bd454d7fcb6bae24eb39847aad03c26adc4
juyingnan <4323145+juyingnan@users.noreply.github.com>
2023-10-28 17:07:40 -0700

    added matching

--- utils/rename.py
+++ utils/rename.py
@@ -2,17 +2,20 @@ import os
 import re
 import requests
 
+
 def sanitize_filename(filename):
     invalid_chars = '<>:"/\\|?*'
     for char in invalid_chars:
         filename = filename.replace(char, ' ')
     return filename.strip()
 
+
 def get_movie_id(filename):
     match = re.search(r'([a-zA-Z]{2,6})-? ?(\d{2,5})', filename)
     if match:
         return f'{match.group(1).upper()}-{match.group(2)}'
     return None
+
 
 def get_movie_info(movie_id):
     search_url = f'http://URL/cn/vl_searchbyid.php?keyword={movie_id}'
@@ -22,11 +25,13 @@ def get_movie_info(movie_id):
         title_match = re.search(r'<title>([a-zA-Z]{1,6}-\d{1,5}.+?) - URL</title>', response.text)
         if not title_match:
             # Try again with this format # title="{movie_id} ">
-            print(f'Failed to find title for {movie_id}. Trying again with a different format.')
+            # print(f'Failed to find title for {movie_id}. Trying again with a different format.')
             title_match = re.search(r'title="([a-zA-Z]{1,6}-\d{1,5}.+?)">', response.text)
         if title_match:
             title = title_match.group(1)
             sanitized_title = sanitize_filename(title)
+            if not sanitized_title.lower().startswith(movie_id.lower()):
+                print('id mismatch, double check the result:')
             return sanitized_title
 
     except requests.RequestException as e:
@@ -34,8 +39,9 @@ def get_movie_info(movie_id):
     print(f'Failed to find title for {movie_id}.')
     return None
 
+
 def main():
-    folder_path = r'PATH'
+    folder_path = r'ROOTPATH'
     rename_list = []
 
     for root, dirs, files in os.walk(folder_path):
@@ -55,6 +61,7 @@ def main():
             f.write(f'{old_name} | {new_name}\n')
 
     print('Done! The rename list has been saved to "rename_list.txt".')
+
 
 if __name__ == '__main__':
     main()
